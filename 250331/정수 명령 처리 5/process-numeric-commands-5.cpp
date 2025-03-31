c6d86c173b3433b411c85c2c26c1dcc8702cc416
gmyoungjune <k93987173@gmail.com>
2025-03-31 12:28:38 +0900

    Title: 정수 명령 처리 5, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250331/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 250331/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main() {
     int N;
     cin >> N;
-
-    vector<int> arr;
-    string command;
-
+    
+    vector<int> stack; 
+    
     for (int i = 0; i < N; i++) {
+        string command;
         cin >> command;
         
         if (command == "push_back") {
             int A;
             cin >> A;
-            arr.push_back(A);
+            stack.push_back(A);  
         } 
         else if (command == "pop_back") {
-            if (!arr.empty()) {
-                arr.pop_back();
+            if (!stack.empty()) {
+                stack.pop_back(); 
             }
         } 
         else if (command == "size") {
-            cout << arr.size() << endl;
+            cout << stack.size() << "\n";  
         } 
         else if (command == "get") {
             int k;
             cin >> k;
-            if (k >= 0 && k < arr.size()) {
-                cout << arr[k] << endl;
-            }
+            if (k >= 1 && k <= stack.size()) {
+                cout << stack[k-1] << "\n";  }
         }
     }
+    
     return 0;
 }

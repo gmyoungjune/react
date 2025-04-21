41bec398e021782a810f1073519a5c6fd73e2f6e
gmyoungjune <k93987173@gmail.com>
2025-04-21 23:15:53 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250421/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+for (int i = 0; i < N; i++) {
+    cout << "HelloWorld";
+cout << endl;
+}
+
+    return 0;
+}
